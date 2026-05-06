1491b0abab69363931b6c869b5b17a366a499d4d
0xjaqbek <jaqbek.eth@gmail.com>
2026-05-06 17:40:13 +0200

    fix: remove basename from BrowserRouter configuration

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/drkolo">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/cennik" element={<Cennik />} />
