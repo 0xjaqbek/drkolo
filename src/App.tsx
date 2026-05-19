e6a797c03fecc585ff199d5fc7945a80ca1c172a
0xjaqbek <jaqbek.eth@gmail.com>
2026-05-19 11:53:33 +0200

    feat: wire up analytics route and page view tracking

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,30 @@ import KalendarzAdmin from "./pages/KalendarzAdmin.tsx";
 import ChatAdmin from "./pages/ChatAdmin.tsx";
 import Kwestionariusz from "./pages/Kwestionariusz.tsx";
 import KwestionariuszOdp from "./pages/KwestionariuszOdp.tsx";
+import Analytics from "./pages/Analytics.tsx";
 import { ChatWidget } from "./components/ChatWidget.tsx";
+import { usePageView } from "./hooks/usePageView.ts";
 
 const queryClient = new QueryClient();
+
+const AppRoutes = () => {
+  usePageView();
+  return (
+    <Routes>
+      <Route path="/" element={<Index />} />
+      <Route path="/cennik" element={<Cennik />} />
+      <Route path="/zlecenie" element={<CreateZlecenie />} />
+      <Route path="/zlecenie/:hash" element={<ZlecenieView />} />
+      <Route path="/rezerwacja" element={<Rezerwacja />} />
+      <Route path="/kalendarz" element={<KalendarzAdmin />} />
+      <Route path="/chat-admin" element={<ChatAdmin />} />
+      <Route path="/kwestionariusz" element={<Kwestionariusz />} />
+      <Route path="/kwestionariusz-odp" element={<KwestionariuszOdp />} />
+      <Route path="/analytics" element={<Analytics />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
@@ -23,18 +44,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/cennik" element={<Cennik />} />
-          <Route path="/zlecenie" element={<CreateZlecenie />} />
-          <Route path="/zlecenie/:hash" element={<ZlecenieView />} />
-          <Route path="/rezerwacja" element={<Rezerwacja />} />
-          <Route path="/kalendarz" element={<KalendarzAdmin />} />
-          <Route path="/chat-admin" element={<ChatAdmin />} />
-          <Route path="/kwestionariusz" element={<Kwestionariusz />} />
-          <Route path="/kwestionariusz-odp" element={<KwestionariuszOdp />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <AppRoutes />
         <ChatWidget />
       </BrowserRouter>
     </TooltipProvider>
